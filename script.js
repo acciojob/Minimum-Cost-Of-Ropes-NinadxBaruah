0818414660864217a8c1ce7b3757d801f958d5c2
NinadxBaruah <117712522+NinadxBaruah@users.noreply.github.com>
2023-02-13 18:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const result = document.getElementById("result");
+
+submit.addEventListener("click", function() {
+  // Split the input string into an array of strings
+  const ropes = input.value.split(',');
+
+  // Convert the array of strings into an array of numbers
+  const ropesArray = ropes.map(rope => parseInt(rope));
+
+  // Sort the array in ascending order
+  ropesArray.sort((a, b) => a - b);
+
+  // Calculate the minimum cost of ropes
+  let cost = 0;
+  while (ropesArray.length > 1) {
+    const first = ropesArray.shift();
+    const second = ropesArray.shift();
+    cost += first + second;
+    ropesArray.unshift(first + second);
+    ropesArray.sort((a, b) => a - b);
+  }
+
+  // Display the minimum cost of ropes
+  result.innerHTML = `Minimum cost of ropes: ${cost}`;
+});
